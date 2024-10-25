7769c6e768c2099eef7515394b95de9154aa97b0
sebawyszkow08 <seba.wyszkow08@gmail.com>
2024-10-25 19:54:42 +0200

    dodanie reszty giery

--- main.py
+++ main.py
@@ -45,10 +45,70 @@ class gracz(postac):
             self.hp +=1
 
 
+        print(f"{self.imie} odpoczywa, ma teraz {self.hp} hp")
+
+    def walka(self,przeciwnik):
+        walka = True
+        while walka:
+            print(f" hp gracza :{self.hp}")
+            print(f" hp {przeciwnik.imie} :{przeciwnik.hp}")
+
+            akcja = input("atak czy ucieczka?")
+            if akcja == "atak":
+                self.atakuj(przeciwnik)
+                if przeciwnik.hp <= 0:
+                    print(f"{self.imie} zabija {przeciwnik.nazwa}")
+                    return True
+                przeciwnik.atakuj(self)
+            
+
+            elif akcja == "ucieczka":
+                print(f"{self.gracz} uciekl z pola bitwy :( ")
+
+                przeciwnik.atakuj(self)
+                
+                walka = False
+
+            else:
+                print("-_- wpisz poprawna komende")
+    
+            if self.hp <= 0:
+                print(f"{self.imie} umiera")
+                return False
+
+
+
+gracz1 = gracz()
+gra = True
+
+while gra:
+    akcja = input("jaka akcja wariacie? zwiedzaj lub odpocznij")
+
+    if akcja == "zwiedzaj":
+        if random.randint(0,1) == 0:
+            print("jaskina")
+        else:
+            przeciwnik1 = przeciwnik(gracz1)
+            print(f"{gracz1.imie} natrafil na przeciwnika {przeciwnik1.imie}")
+            gra = gracz1.walka(przeciwnik1)
+    elif akcja == "odpoczynek":
+        gracz1.odpoczynek()
+
+
+    else:
+        print("nieznana akcja")
+
         
 
 
-    
+
+
+
+
+
+
+
+
 
 
 
