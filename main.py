f46c04dfa83c9b39e0ecdf1d4d6eb3259962f599
sebawyszkow08 <seba.wyszkow08@gmail.com>
2024-10-25 19:13:12 +0200

    koeljna klasa

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class postac:
     def __init__(self):
         self.imie = ""
         self.hp = 1
-        self.maxhp = 1
+        self.maxhp = 10
 
     def atakuj(self,przeciwnik):
         print(f"{self.imie} atakuje!")
@@ -14,8 +14,41 @@ class postac:
         if atak == 0:
             print(f"{przeciwnik.nazwa} unika ataku")
 
+        else:
+            print(f"{self.imie} trafił, zadał {atak} obrażeń")
+            przeciwnik.hp -= atak
 
 
+
+
+class przeciwnik(postac):
+    def __init__(self,gracz):
+        super().__init__()
+        nazwy = ["goblin","ryboczłowiek","bezdomny"]
+        self.nazwa = random.choice(nazwy)
+        self.hp = random.randint(1,(gracz.hp // 2))
+        
+
+    
+class gracz(postac):
+    def __init__(self):
+        super().__init__()
+        self.hp = 10
+        self.maxhp = 10
+
+
+    #def pobracnazwe(self):
+        self.imie = input("podaj swoje imie")
+
+    def odpoczynek(self):
+        if self.hp < self.maxhp:
+            self.hp +=1
+
+
+        
+
+
+    
 
 
 
